73fdce76e4fdf38528bae954aff66af849a14d31
cacauisadog <cbcaue@protonmail.com>
2022-08-16 08:52:47 -0300

    format files for consistency

--- 13-roman_to_integer.py
+++ 13-roman_to_integer.py
@@ -1,39 +1,41 @@
 # https://leetcode.com/problems/roman-to-integer/
 
 symbols = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000,
-    'IV': 4,
-    'IX': 9,
-    'XL': 40,
-    'XC': 90,
-    'CD': 400,
-    'CM': 900,
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+    "IV": 4,
+    "IX": 9,
+    "XL": 40,
+    "XC": 90,
+    "CD": 400,
+    "CM": 900,
 }
+
 
 def roman_to_int(s: str) -> int:
     num = 0
     i = 0
 
-    while i < len(s): 
-        double_letter = ''
+    while i < len(s):
+        double_letter = ""
         if i < len(s) - 1:
-            double_letter = s[i] + s[i+1]
+            double_letter = s[i] + s[i + 1]
 
         if double_letter in symbols:
             num += symbols[double_letter]
             i += 2
         else:
-            num += symbols[s[i]] 
+            num += symbols[s[i]]
             i += 1
 
     print(num)
 
-roman_to_int('III')
-roman_to_int('LVII')
-roman_to_int('MCMXCIV')
+
+roman_to_int("III")
+roman_to_int("LVII")
+roman_to_int("MCMXCIV")
--- 234-palindrome_linked_list.py
+++ 234-palindrome_linked_list.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/palindrome-linked-list/
 
+
 def is_palindrome(head):
     complete_list = []
     while head:
@@ -11,14 +12,13 @@ def is_palindrome(head):
     if len_complete_list == 1:
         return True
 
-    half_of_list = int(len_complete_list/2)
+    half_of_list = int(len_complete_list / 2)
     first_half = complete_list[0:half_of_list]
 
     if len_complete_list % 2 == 0:
         second_half = complete_list[half_of_list::1]
 
     else:
-        second_half = complete_list[(half_of_list)+1::1]
+        second_half = complete_list[(half_of_list) + 1:: 1]
 
     return first_half == second_half[::-1]
-
